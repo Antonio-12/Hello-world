3f425e6fd7b23967679dedc0a0ba28393ad95ff0
Antonio-12 <78031057+Antonio-12@users.noreply.github.com>
2021-01-27 09:09:43 -0800

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+ /****************************************
+  * Hello
+  ****************************************
+  * Student Name (Lazaro Camero)
+  * 01/27/2021
+  * CMSC-255-003
+  ****************************************/
+ 
+ public class Hello {
+     public static void main(String[] args) {
+      System.out.println("<Lazaro Camero>");
+      System.out.println("<Hello>");
+      System.out.println("<Run a Java program with the use of related application>");
+      System.out.println("<01/27/2021>");
+      System.out.println("<CMSC-255-003>");
+      System.out.println();
+     }
+ }
+ 
